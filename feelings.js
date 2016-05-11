09672bf600a572ddf4160c33e78f519272190da4
mlindsay-cmd <mlindsay@cmdagency.com>
2016-05-11 11:33:44 -0700

    added feeling.js

---
+++ feelings.js
@@ -0,0 +1,8 @@
+(function(){
+    var feeling = ["sensational", "stupendous", "obsessive compulsive"];
+
+    var rand = Math.floor(Math.random()*(arr.length+1));
+
+    console.log(`I am feeling rand`);
+
+})()

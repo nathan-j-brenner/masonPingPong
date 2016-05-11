ecc6532f9531f470c0e9c7850b6ae30d7a4d434e
Nathan Brenner <nbrenner@cmdagency.com>
2016-05-11 14:37:01 -0700

    made the challenge more difficult

--- feelings.js
+++ feelings.js
@@ -1,11 +1,13 @@
 (function(){
-    var feelings = ["sensational", "stupendous", "obsessive compulsive"];
+    var feelings = {
+        "Monday": ["sensational", "stupendous", "obsessive compulsive"];
+    }
 
     var rand;
 
     for (var i = 0; i < 5; i++) {
         rand = Math.floor(Math.random()*(feelings.length+1));
-        console.log(`I am feeling ${feelings[rand}.`);
+        console.log(`I am feeling ${feelings[rand]}.`);
     }
 
 })()

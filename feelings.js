0669b8dd10db018cd674c2781c34762976fdd6cb
Nathan Brenner <nbrenner@cmdagency.com>
2016-05-11 14:18:26 -0700

    arr was undefined, and should reference the feelings array

--- feelings.js
+++ feelings.js
@@ -1,7 +1,7 @@
 (function(){
     var feelings = ["sensational", "stupendous", "obsessive compulsive"];
 
-    var rand = Math.floor(Math.random()*(arr.length+1));
+    var rand = Math.floor(Math.random()*(feelings.length+1));
 
     console.log(`I am feeling rand`);
 
